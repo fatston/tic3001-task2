7d900f6625c8330e109c4b8c298ab8034b99aac8
fatston <cliftonkor@gmail.com>
2023-04-22 13:42:25 +0800

    loool

---
+++ routes/api/pokemon.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const pokemonDataPath = path.join(__dirname, 'pokemonData.json');
+
+router.get('/', function(req,res,next) {
+    const pokemonList = readPokemonData();
+    res.json(pokemonList);
+})
+
+router.get('/search', function(req, res, next) {
+    const pokemonList = readPokemonData();
+    const queryId = req.query.id;
+    const queryType = req.query.type;
+
+    if (queryId) {
+        const idAsNumber = parseInt(queryId); // convert to number
+        const matchingPokemon = pokemonList.find(p => p.number === idAsNumber);
+        if (matchingPokemon) {
+            res.json(matchingPokemon);
+            return;
+        } else {
+            res.status(404).json({'error': 'couldn\'t find pokemon with id ' + queryId});
+            return;
+        }
+    }
+
+    if (queryType) {
+        const matchingPokemon = pokemonList.filter(p => p.type1 === queryType || p.type2 === queryType);
+        if (matchingPokemon.length > 0) {
+            res.json(matchingPokemon);
+            return;
+        } else {
+            res.status(404).json({'error': 'couldn\'t find pokemon with type ' + queryType});
+            return;
+        }
+    }
+
+    res.status(400).json({'error': 'please enter id or type'});
+
+});
+
+/* POST to insert pokemon */
+router.post('/', function(req, res, next) {
+    const { number, name, type1, type2 } = req.body;
+    console.log('request body: ', req.body);
+    console.log('number:', number);
+    console.log('name:', name);
+    console.log('type1:', type1);
+    console.log('type2:', type2);
+
+    if (!number || !name || !type1 || !type2) {
+        res.json({"error": "please make sure u have number, name, type1, type2. Thanks"});
+        return;
+    }
+
+    const parsedNumber = parseInt(number);
+    const newPokemon = {number: parsedNumber, name, type1, type2};
+    try {
+        // read existing pokemon data from the JSON file
+        const pokemonData = readPokemonData();
+        pokemonData.push(newPokemon);
+        // write the updated pokemon data back to the JSON file
+        writePokemonData(pokemonData);
+        res.json({ "msg": "inserted pokemon", "newPokemon": newPokemon });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ "error": "failed to insert pokemon" });
+    }
+});
+
+/* put to update pokemon */
+router.put('/:number', function(req, res, next) {
+    const { name, type1, type2 } = req.body;
+    const number = parseInt(req.params.number);
+
+    try {
+        const pokemonData = readPokemonData();
+        const index = pokemonData.findIndex(p => p.number === number);
+
+        if (index < 0) {
+            res.status(404).json({ "error": "pokemon not found" });
+            return;
+        }
+
+        const updatedPokemon = { number, name, type1, type2 };
+        pokemonData[index] = updatedPokemon;
+        writePokemonData(pokemonData);
+
+        res.json({ "msg": "updated pokemon", "updatedPokemon": updatedPokemon });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ "error": "failed to update pokemon" });
+    }
+});
+
+router.delete('/:number', function(req, res, next) {
+    const number = parseInt(req.params.number);
+
+    try {
+        const pokemonData = readPokemonData();
+        const index = pokemonData.findIndex(p => p.number === number);
+
+        if (index < 0) {
+            res.status(404).json({ "error": "pokemon not found" });
+            return;
+        }
+
+        const deletedPokemon = pokemonData[index];
+        pokemonData.splice(index, 1);
+        writePokemonData(pokemonData);
+
+        res.json({ "msg": "deleted pokemon", "deletedPokemon": deletedPokemon });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ "error": "failed to delete pokemon" });
+    }
+});
+
+function readPokemonData() {
+    return JSON.parse(fs.readFileSync(pokemonDataPath, 'utf-8'));
+}
+
+
+function writePokemonData(pokemonList) {
+    const pokemonData = JSON.stringify(pokemonList);
+    fs.writeFileSync(pokemonDataPath, pokemonData);
+}
+
+module.exports = router;
---
+++ test/test.js
@@ -0,0 +1,198 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Baseline Test', function() {
+    it('should return true', function() {
+        expect(true).to.equal(true);
+    });
+});
+
+describe('API Tests for GET', function() {
+    describe('GET /pokemon', function() {
+        it('should return a JSON object with status code 200', function(done) {
+            chai.request(app)
+                .get('/api/pokemon')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('array');
+                    expect(res.body[0]).to.have.property('number');
+                    expect(res.body[0]).to.have.property('name');
+                    expect(res.body[0]).to.have.property('type1');
+                    expect(res.body[0]).to.have.property('type2');
+                    done();
+                });
+        });
+    });
+
+    describe('GET /api/pokemon/search', function() {
+        it('should return a JSON object with status code 200 when searching by id', function(done) {
+            chai.request(app)
+                .get('/api/pokemon/search?id=1')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.number).to.equal(1);
+                    expect(res.body.name).to.be.a('string');
+                    expect(res.body.type1).to.be.a('string');
+                    expect(res.body.type2).to.be.a('string');
+                    done();
+                });
+        });
+
+        it('should return a JSON array with status code 200 when searching by type', function(done) {
+            chai.request(app)
+                .get('/api/pokemon/search?type=Electric')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('array');
+                    expect(res.body[0]).to.have.property('number');
+                    expect(res.body[0]).to.have.property('name');
+                    expect(res.body[0]).to.have.property('type1');
+                    expect(res.body[0]).to.have.property('type2');
+                    done();
+                });
+        });
+
+        it('should return an error message with status code 400 when no search parameter is provided', function(done) {
+            chai.request(app)
+                .get('/api/pokemon/search')
+                .end(function(err, res) {
+                    expect(res).to.have.status(400);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.error).to.equal('please enter id or type');
+                    done();
+                });
+        });
+
+        it('should return an error message with status code 404 when searching by id for a non-existent pokemon', function(done) {
+            chai.request(app)
+                .get('/api/pokemon/search?id=9999')
+                .end(function(err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.error).to.equal('couldn\'t find pokemon with id 9999');
+                    done();
+                });
+        });
+
+        it('should return an error message with status code 404 when searching by type for a non-existent pokemon', function(done) {
+            chai.request(app)
+                .get('/api/pokemon/search?type=Rockk')
+                .end(function(err, res) {
+                    expect(res).to.have.status(404);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.error).to.equal('couldn\'t find pokemon with type Rockk');
+                    done();
+                });
+        });
+    });
+});
+
+describe('API Tests for CRUD', function() {
+    describe('Insert, Update, and Delete Pokemon', function() {
+        let pokemonId;
+
+        it('should insert a new Pokemon with status code 200', function(done) {
+            chai.request(app)
+                .post('/api/pokemon')
+                .send({
+                    number: 6969,
+                    name: 'cliftonTestPokemonWrong',
+                    type1: 'Flying',
+                    type2: 'Rock'
+                })
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body.msg).to.equal('inserted pokemon');
+                    expect(res.body.newPokemon).to.have.property('number');
+                    expect(res.body.newPokemon.number).to.equal(6969);
+                    pokemonId = res.body.newPokemon.number;
+                    done();
+                });
+        });
+
+        it('should find the newly created Pokemon by ID with status code 200', function(done) {
+            chai.request(app)
+                .get(`/api/pokemon/search?id=${pokemonId}`)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body.number).to.equal(6969);
+                    expect(res.body.name).to.equal('cliftonTestPokemonWrong');
+                    expect(res.body.type1).to.equal('Flying');
+                    expect(res.body.type2).to.equal('Rock');
+                    done();
+                });
+        });
+
+        it('should update the Pokemon with status code 200', function(done) {
+            chai.request(app)
+                .put(`/api/pokemon/${pokemonId}`)
+                .send({
+                    name: 'cliftonTestPokemon',
+                    type1: 'Rock',
+                    type2: 'Flying'
+                })
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body.msg).to.equal('updated pokemon');
+                    expect(res.body.updatedPokemon).to.have.property('number');
+                    expect(res.body.updatedPokemon.number).to.equal(6969);
+                    done();
+                });
+        });
+
+        it('should find the updated Pokemon by ID with status code 200', function(done) {
+            chai.request(app)
+                .get(`/api/pokemon/search?id=${pokemonId}`)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body.number).to.equal(6969);
+                    expect(res.body.name).to.equal('cliftonTestPokemon');
+                    expect(res.body.type1).to.equal('Rock');
+                    expect(res.body.type2).to.equal('Flying');
+                    done();
+                });
+        });
+
+        it('should delete the Pokemon with status code 200', function(done) {
+            chai.request(app)
+                .delete(`/api/pokemon/${pokemonId}`)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body.msg).to.equal('deleted pokemon');
+                    expect(res.body.deletedPokemon).to.have.property('number');
+                    expect(res.body.deletedPokemon.number).to.equal(6969);
+                    done();
+                });
+        });
+
+        it('should not find the deleted Pokemon by ID with status code 404', function(done) {
+            chai.request(app)
+                .get(`/api/pokemon/search?id=${pokemonId}`)
+                .end(function(err, res) {
+                expect(res).to.have.status(404);
+                expect(res).to.be.json;
+                expect(res.body).to.have.property('error');
+                expect(res.body.error).to.equal(`couldn't find pokemon with id ${pokemonId}`);
+                done();
+            });
+        });
+    });
+});
+
+module.exports = app;
